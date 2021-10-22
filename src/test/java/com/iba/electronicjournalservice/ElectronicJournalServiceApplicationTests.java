31f20c20a776522bf54acf82fa48452bf4de9732
i.khaletski <khaletskiilya@gmail.com>
2021-10-22 10:37:39 +0300

    Created project

---
+++ src/test/java/com/iba/electronicjournalservice/ElectronicJournalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iba.electronicjournalservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectronicJournalServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
